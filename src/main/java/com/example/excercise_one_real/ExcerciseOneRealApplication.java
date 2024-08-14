4c0c7eeae24f4ac99672c9b3d1f896606fec694c
scalleja <scalleja12345@gmail.com>
2024-08-14 11:34:28 -0400

    finished glab

---
+++ src/main/java/com/example/excercise_one_real/ExcerciseOneRealApplication.java
@@ -0,0 +1,13 @@
+package com.example.excercise_one_real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcerciseOneRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcerciseOneRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/excercise_one_real/ExcerciseOneRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excercise_one_real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcerciseOneRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
